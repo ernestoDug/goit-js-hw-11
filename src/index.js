0e4aee49e13c1cada3917812d162f2b1b22d03bb
Ev Bludov <120216330+ernestoDug@users.noreply.github.com>
2023-07-07 13:15:31 +0300

    4

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import axios, { isCancel, AxiosError } from 'axios';
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+
+const MY_KEY = "28539247-0afb9c376c93f2bc11eaacc3c";
+const BASE_URL = "https://pixabay.com/api/";
+
+async function fetchBreeds() {
+    // запит
+    const responseImg  = await axios
+      .get(`${BASE_URL}?api-key=${MY_KEY}`, {
+        // Headers: {
+        //   'x-api-key': `${MY_KEY}`
+        // }
+      })
+              return response;
+      }
+    //   .then(selecter)
+    //   .catch(error => {
+    //     Notify.warning(`❌ ОТАКОЇ, КОШАКИ  РОЗБІГЛИСЯ`);
+        // **********************************************************************
+      
